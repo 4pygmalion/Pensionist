cec3ef72f94d87fa567f5463833a84b47b9d2261
4pygmalion <4pygmalion@naver.com>
2024-11-16 19:41:51 +0900

    Package (#22)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="quant",
+    author="Ho Heon Kim",
+    author_email="hoheon0509@gmail.com",
+    version="0.2.1",
+    url="Thttps://github.com/4pygmalion/Pensionist",
+    package_dir={"quant": "quant"},
+    python_requires=">=3.11",
+)
